9419c5d9b8be1658ef3fb79a0466f5d27534c0ab
Rodrigo Nunez <vsurak@gmail.com>
2019-03-30 14:17:36 -0600

    Add dynamic-chors tempalte

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ const noteDuration = 1200; // 1.2 seconds note
 const song : number[][] = []; // [[midiNote, duration, timeline]]
 const minNote = 20;
 const maxNote = 120;
-const AMOUNT_OF_NOTES = 10;
+const AMOUNT_OF_NOTES = 30;
 
 const playSong = new Promise<number[][]>((resolve, reject) => {
   for (let i = 0; i < AMOUNT_OF_NOTES; i++) { // intentando que el random me lleve a 3 notas overlap en los mismos 2 segundos
--- midi-player.ts
+++ midi-player.ts
@@ -4,28 +4,36 @@
  * Date: 03/21/2019
  * Description: main program file
  */
+
+import { MIDI } from 'midi.js'; 
 const TIME_POSITION = 2;
 
 export class MidiPlayer {
   private song: number[][];
 
   public constructor(pSong: number[][]) {
-    this.sortSong(pSong);
-    this.song = pSong;
+    this.song = this.sortSong(pSong);
   }
 
   public play() : void {
+    // MIDI.loadPlugin({
+    //   instrument: "acoustic_grand_piano", // or the instrument code 1 (aka the default)
+    //   instruments: [ "acoustic_grand_piano", "acoustic_guitar_nylon" ], // or multiple instruments
+    //   onsuccess: function() {
+    //     console.log("Connected");
+    //    }
+    // });
     console.log(this.song);
   }
 
-  private sortSong(pSong: number[][]) : void {
-    pSong.sort((noteA: number[], noteB: number[]) => {
+  private sortSong(pSong: number[][]) : number[][] {
+    return pSong.sort((noteA: number[], noteB: number[]) => {
       
-      if (noteA[TIME_POSITION] < noteB[TIME_POSITION]) {
+      if (noteA[TIME_POSITION] > noteB[TIME_POSITION]) {
         return 1;
       } 
 
-      if (noteA[TIME_POSITION] > noteB[TIME_POSITION]) {
+      if (noteA[TIME_POSITION] < noteB[TIME_POSITION]) {
         return -1;
       } 
 
